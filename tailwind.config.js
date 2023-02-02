161ae05bc46d0136a4e93d4f7d1c8d87524fa647
weshsnr <ndungugeoffrey11@gmail.com>
2023-02-02 11:58:31 +0300

    added google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'hero-one': 'url("../images/bgimage2.jpg")'
+      },
+      colors: {
+        sil: {
+          secondary: '#BADA55',
+          DEFAULT: '#0092FF',
+          accent: '#2c1e55'
+        }
+      },
+      fontFamily: {
+        montserrat: 'Montserrat, sans-serif',
+        raleway: 'Raleway, serif'
+      }
+    }
   },
   plugins: []
 }
