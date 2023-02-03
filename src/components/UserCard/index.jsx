ecba119cf1c8c864a4571faa025d9e9c24cd1944
weshsnr <ndungugeoffrey11@gmail.com>
2023-02-03 09:41:42 +0300

    updated usercard component styling

--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -4,12 +4,13 @@ import { FolderPlusIcon, EnvelopeIcon, TagIcon } from '@heroicons/react/24/solid
 const UserCard = ({ user }) => {
   const { name, email } = user
   return (
-    <div className='mb-32  hover:scale-105 transition-all duration-300 ease-in-out'>
+    <div className='mb-36  hover:scale-105 transition-all duration-300 ease-in-out'>
       {/* image div */}
       <div className='rounded-md relative group cursor-pointer'>
         <img
           src='https://picsum.photos/200/300'
           alt='user_image'
+          loading='lazy'
           className='rounded-xl w-full hover:scale-105 transition-all duration-300 ease-in-out '
         />
         {/* card info */}
@@ -24,13 +25,13 @@ const UserCard = ({ user }) => {
             <span className='text-emerald-500'>{name} </span>
           </div>
 
-          <div className='flex items-center mb-8'>
+          <div className='flex items-center mb-7'>
             <EnvelopeIcon className='h-5 w-5 text-sil-dark mr-3 ' />{' '}
             <span className='text-sil-dark'>{email} </span>
           </div>
 
-          <div className='m-auto hidden items-center w-20 group-hover:block  '>
-            <button className='mx-auto view-btn'> View</button>
+          <div className='m-auto hidden items-center w-20 group-hover:block mb-4  '>
+            <button className='mx-auto view-btn'> view</button>
           </div>
         </div>
       </div>
